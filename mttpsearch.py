0580f5f79b534d09986de33afbe1ddd81b2faafd
Tripp Barker <colemanbarker@gmail.com>
2023-06-14 19:36:12 -0400

    term txt docs

--- mttpsearch.py
+++ mttpsearch.py
@@ -1,6 +1,6 @@
 from googlesearch import search
 
-query = 'python'
+query = '"clinical project manager" +"remote" -"research" -"trial" -"trials"after:2023-06-14 site:"www.linkedin.com"'
 
-for i in search(query, tld="com", num=10, stop=10, pause=2):
+for i in search(query, tld="com", num=10, stop=20, pause=2):
     print(i)
